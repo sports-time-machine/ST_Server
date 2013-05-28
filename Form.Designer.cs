2beeffd514b41fda6eeca64c481e1afe38a330d8
Minako <minako@eurekacomputer.jp>
2013-05-28 18:24:17 +0900

    UDP connection now.

--- Form.Designer.cs
+++ Form.Designer.cs
@@ -28,12 +28,77 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.buttonServer = new System.Windows.Forms.Button();
+			this.buttonClient = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(197, 227);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "送信";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Visible = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 202);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(260, 19);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "Hello World";
+			this.textBox1.Visible = false;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// buttonServer
+			// 
+			this.buttonServer.Location = new System.Drawing.Point(12, 12);
+			this.buttonServer.Name = "buttonServer";
+			this.buttonServer.Size = new System.Drawing.Size(75, 23);
+			this.buttonServer.TabIndex = 3;
+			this.buttonServer.Text = "サーバー";
+			this.buttonServer.UseVisualStyleBackColor = true;
+			this.buttonServer.Click += new System.EventHandler(this.buttonServer_Click);
+			// 
+			// buttonClient
+			// 
+			this.buttonClient.Location = new System.Drawing.Point(197, 12);
+			this.buttonClient.Name = "buttonClient";
+			this.buttonClient.Size = new System.Drawing.Size(75, 23);
+			this.buttonClient.TabIndex = 4;
+			this.buttonClient.Text = "クライアント";
+			this.buttonClient.UseVisualStyleBackColor = true;
+			this.buttonClient.Click += new System.EventHandler(this.buttonClient_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.buttonClient);
+			this.Controls.Add(this.buttonServer);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button buttonServer;
+		private System.Windows.Forms.Button buttonClient;
 	}
 }
 
--- Form.cs
+++ Form.cs
@@ -6,14 +6,117 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
 
 namespace ST_Server
 {
 	public partial class Form1 : Form
 	{
+		const int ST_UDP_PORT = 38707;
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private static Form1 self;
+		private static bool quit_receive = false;
+
+		public static void ReceiveProc()
+		{
+			Debug.Print("begin receive thread");
+			while (!quit_receive)
+			{
+				self.Receive();
+				Thread.Sleep(1);
+			}
+			Debug.Print("end of receive thread");
+		}
+
+		System.Net.Sockets.UdpClient udp;
+
+		string out_text;
+
+		void UpdateText()
+		{
+			textBox1.Text = out_text;
+		}
+
+		delegate void UpdateTextDelegate();
+
+		private void Receive()
+		{
+			var enc = System.Text.Encoding.UTF8;
+
+			System.Net.IPEndPoint remoteEP = null;
+			if (udp.Available>0)
+			{
+				byte[] rcvBytes = udp.Receive(ref remoteEP);
+				if (rcvBytes.Length>0)
+				{
+					Debug.Print(rcvBytes.Length.ToString()+" bytes received");
+					string rcvMsg = enc.GetString(rcvBytes);
+					out_text = rcvMsg;
+					self.Invoke(new UpdateTextDelegate(UpdateText));
+				}
+			}
+			else
+			{
+				Debug.Print("no received");
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			send_data();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			self = this;
+		}
+
+		private void buttonServer_Click(object sender, EventArgs e)
+		{
+			textBox1.Visible = true;
+			button1.Visible = true;
+
+			udp = new System.Net.Sockets.UdpClient();
+		}
+
+		Thread rcv_thread;
+
+		private void buttonClient_Click(object sender, EventArgs e)
+		{
+			textBox1.Visible = true;
+			button1.Visible = false;
+
+			udp = new System.Net.Sockets.UdpClient(ST_UDP_PORT);
+
+			rcv_thread = new Thread(ReceiveProc);
+			rcv_thread.Start();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			quit_receive = true;
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			send_data();
+		}
+
+		void send_data()
+		{
+			var enc = System.Text.Encoding.UTF8;
+			string remoteHost = "localhost";
+			
+			string sendMsg = textBox1.Text;
+			byte[] sendBytes = enc.GetBytes(sendMsg);
+
+			udp.Send(sendBytes, sendBytes.Length, remoteHost, ST_UDP_PORT);
 		}
 	}
 }
